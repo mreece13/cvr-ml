e363b4c8e1e81d04730a31fa5f2e17f81ea990af
Mason Reece <masonreece1317@gmail.com>
2025-10-13 13:11:05 -0400

    slurm stuff

--- scheduler.sh
+++ scheduler.sh
@@ -2,10 +2,10 @@
 
 # Job Flags
 #SBATCH -p mit_normal_gpu
-#SBATCH -c 8
+#SBATCH -c 16
 #SBATCH --mem=128G
 #SBATCH --gres=gpu:h200:1
-#SBATCH --signal=SIGUSR1@180
+#SBATCH --signal=SIGUSR1@360
 #SBATCH --time=05:30:00
 
 # Set up environment
@@ -15,5 +15,7 @@ module load cuda/12.4.0
 mamba activate cvr-ml
 
 # Run your application
-# srun python main_lightning.py --data data/colorado.parquet --epochs 10 --batch-size=512
-srun python main_lightning.py --data data/colorado.parquet --batch-size=512 --eval-only True
+srun python main_lightning.py --data data/colorado.parquet --batch-size=512 --latent-dims 1 --epochs 20
+# srun python main_lightning.py --data data/colorado.parquet --batch-size=512 --latent-dims 2 --epochs 20
+srun python main_lightning.py --data data/colorado.parquet --batch-size=512 --eval-only True --latent-dims 1
+#srun python main_lightning.py --data data/colorado.parquet --batch-size=512 --eval-only True --latent-dims 2
