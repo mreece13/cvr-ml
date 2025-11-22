767ee0e7884b487afae35a884af9319be09022da
Mason Reece <mpreece@mit.edu>
2025-11-22 15:00:40 -0500

    Update create_sample.R

--- data/create_sample.R
+++ data/create_sample.R
@@ -7,17 +7,16 @@ library(here)
 
 here::i_am("data/create_sample.R")
 
-a = read_parquet(here("data/all.parquet"))
+a = bind_rows(
+  read_parquet(here("data/all.parquet")),
+  read_parquet(here("data/colorado.parquet"))
+)
+
+write_parquet(a, here("data/combined.parquet"))
 
 a |> 
   semi_join(
     distinct(a, state, county_name, cvr_id) |> slice_sample(n=10000, by = c(state, county_name)),
     join_by(state, county_name, cvr_id)
   ) |> 
-  write_parquet(here("data/all_sample.parquet"))
-
-bind_rows(
-  a,
-  read_parquet(here("data/colorado.parquet"))
-) |> 
-  write_parquet(here("data/combined.parquet"))
+  write_parquet(here("data/combined_sample.parquet"))
